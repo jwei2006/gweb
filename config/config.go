ef51ebbb346e8355f928ccbaf33570aed0463514
unknown <jiwei_m@cyou-inc.com>
2018-12-15 21:55:16 +0800

    m1

--- config/config.go
+++ config/config.go
@@ -30,13 +30,14 @@ const Key_RequestId = "X-REQUEST-ID"
 const Key_Open = "k_open"
 const Openid = "openid"
 const Key_UserId = "k_userId"
+const Key_UserName = "k_userName"
 
 const Key_principal string = "k_principal"
 const Value_principal string = "PINGX"
 const Key_Tenant string = "k_tenant"
 const Value_tenant string = "PX"
-const Key_CompanyId string = "k_company_id"
-const Key_Company_Type string = "k_company_type"
+const Key_CompanyId string = "k_companyId"
+const Key_Company_Type string = "k_companyType"
 const Key_Business string = "k_business"
 
 var Conf SysConfig = SysConfig{}
