7e05edc1fbd6fb9bb3aeb8907bc9999624c81636
unknown <jiwei_m@cyou-inc.com>
2018-12-16 18:00:10 +0800

    import

--- sys/sessionUtil.go
+++ sys/sessionUtil.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"github.com/jwei2006/gweb/config"
+	"github.com/jwei2006/gweb/sys"
 )
 
 func GetSession(c *gin.Context, key string) string {
@@ -20,20 +21,18 @@ func SetSession(c *gin.Context, key, val string)  {
 	session.Save()
 }
 
-func GetCtx(c *gin.Context) Context {
+func GetCtx(c *gin.Context) sys.Context {
 	ctx, exists := c.Get(config.Key_Ctx)
 	if !exists {
 		Error().Fatalln(errors.New("上下文环境不存在"))
-		_ctx := new(Context)
-		_ctx.UserId = ""
+		_ctx := new(sys.Context)
 		return *_ctx
 	}
-	v, ok := ctx.(Context)
+	v, ok := ctx.(sys.Context)
 	if ok{
 		return v
 	}
-	_ctx := new(Context)
-	_ctx.UserId = ""
+	_ctx := new(sys.Context)
 	return *_ctx
 }
 
